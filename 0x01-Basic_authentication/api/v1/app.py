4e32b68017e836eb4f72e9409409cbf062b1b334
adesanyaolu <sanya4reel@gmail.com>
2023-03-07 11:25:47 +0100

    Unauthorized

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,13 +18,9 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 def not_found(error) -> str:
     """ Not found handler
     """
-    return jsonify({"error": "Unauthorized"}), 401
+    return jsonify({"error": "Unauthorized"}), 401 
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Forbidden"}), 403
+
 
 
 if __name__ == "__main__":
